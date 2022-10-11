f09a3e6168023c916bc188d1adf880c36265aa32
Juni <junitrigger@gmail.com>
2022-10-11 12:10:58 +0200

    feat: set up express,apollo,mongodb backend

--- backend/web/index.js
+++ backend/web/index.js
@@ -1,68 +1,32 @@
-const { ApolloServer, gql } = require('apollo-server');
+const express = require("express")
+const { ApolloServer, gql } = require("apollo-server-express");
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
+const mongoose = require("mongoose");
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+async function startServer() {
+  const app = express()
+  const apolloServer = new ApolloServer({ 
+    typeDefs: typeDefs,
+    resolvers: resolvers
+   });
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
+   await apolloServer.start();
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
+   apolloServer.applyMiddleware({ app: app });
 
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
+   app.use((req,res) => {
+    res.send("Hello from express apollo server")
+   });
 
-  // Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers = {
-    Query: {
-      books: () => books,
-    },
-  };
+   await mongoose.connect("mongodb://admin:password@it2810-34.idi.ntnu.no:27017/admin", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  console.log("Mongoose connected...")
 
-const {
-ApolloServerPluginLandingPageLocalDefault
-} = require('apollo-server-core');
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-typeDefs,
-resolvers,
-csrfPrevention: true,
-cache: 'bounded',
-/**
- * What's up with this embed: true option?
- * These are our recommended settings for using AS;
- * they aren't the defaults in AS3 for backwards-compatibility reasons but
- * will be the defaults in AS4. For production environments, use
- * ApolloServerPluginLandingPageProductionDefault instead.
-**/
-plugins: [
-    ApolloServerPluginLandingPageLocalDefault({ embed: true }),
-],
-});
-
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-console.log(`🚀  Server ready at ${url}`);
-});
+   app.listen({ port: 4000 }, () => 
+     console.log(`Server is running at http://localhost:4000${apolloServer.graphqlPath}`)
+   );
+}
+startServer()
