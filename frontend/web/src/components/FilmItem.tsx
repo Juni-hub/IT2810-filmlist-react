4f7300c9c014b9768ff441993c16521e3f954e1b
Linn Marie <lmpeders@stud.ntnu.no>
2022-10-25 13:04:51 +0200

    fix: made text bigger

--- frontend/web/src/components/FilmItem.tsx
+++ frontend/web/src/components/FilmItem.tsx
@@ -8,10 +8,11 @@ export const ShowFilmItem: React.FC<CollectionCreateFilmProps> = ({
 }) => {
 
   return(
-    <Modal title={film.title} cancelText="Close" open={open} onCancel={onCancel} footer={null}>
-      <p>Year: {film.year? film.year: ""} </p>
-      <p>Cast: {film.cast.length === 0? "No cast is registered for this film": film.cast.map((el) => el + ", ")}</p>
-      <p>Genres: {film.genres.length === 0? "No genre is registered for this film" : film.genres.map((el) => el + ", ")}</p>
+    <Modal title={"Chosen film"} cancelText="Close" open={open} onCancel={onCancel} footer={null}>
+      <p style={{fontSize: 30}}> {film.title? film.title: ""} </p>
+      <p style={{fontSize: 20}}>Year: {film.year? film.year: ""} </p>
+      <p style={{fontSize: 20}}>Cast: {film.cast.length === 0? "No cast is registered for this film": film.cast.map((el) => el + ", ")}</p>
+      <p style={{fontSize: 20}}>Genres: {film.genres.length === 0? "No genre is registered for this film" : film.genres.map((el) => el + ", ")}</p>
     </Modal>
   );
 }
