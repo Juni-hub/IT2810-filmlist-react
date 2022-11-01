1a94b0024a56e2e909dee749a7a12a32403ba29b
FridaK-sys <fridark@stud.ntnu.no>
2022-11-01 14:17:00 +0100

    doc: added more comments

--- frontend/web/src/helpers/helpers.tsx
+++ frontend/web/src/helpers/helpers.tsx
@@ -2,6 +2,10 @@ import { Select } from "antd";
 
 const { Option } = Select;
 const optionItems = ["Drama", "Documentary", "Sports", "Silent", "Adventure", "Western", "Romance", "War", "Comedy", "Horror", "Historical", "Animated"]
+
+/** 
+* Helper methods for setting genres and year
+*/
 export const optionList: React.ReactNode[] = [];
 
 optionItems.forEach((e) => {
--- frontend/web/src/queries/filmQueries.ts
+++ frontend/web/src/queries/filmQueries.ts
@@ -1,5 +1,9 @@
 import { gql } from '@apollo/client'
 
+
+/** 
+* Query for searing for films
+*/
 export const SEARCH_FILMS = gql`
   query getPosts($limit: Int!, $offset: Int!, $titleFilter: String, $genreFilter: String, $yearFilter: Int, $sorting: Int) {
     getFilteredPosts(limit: $limit, offset: $offset, titleFilter: $titleFilter, genreFilter: $genreFilter, yearFilter: $yearFilter, sorting: $sorting) {
@@ -11,7 +15,9 @@ export const SEARCH_FILMS = gql`
     }
   },
 `;
-
+/** 
+* Query for adding a film
+*/
 export const ADD_FILM = gql`
   mutation createPost($title: String!, $year: Int, $cast: [String], $genres: [String]) {
     createPost(title: $title, year: $year, cast: $cast, genres: $genres) {
